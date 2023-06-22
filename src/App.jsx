2372c6e4982ee17f397204902cb767d2fb7cf281
thevid <thevid@localhost.localdomain>
2023-06-22 22:37:46 +0800

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,25 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Home from "./home/Home.jsx"
+import List from "./list/List.jsx"
+import {BrowserROuter, Routes, Route} from "react-router-dom"
+
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  <BrowserROuter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/hotels" element={<List/>} />
+      <Route path="/hotels/:id" element={<Hotel/>} />:
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    </Routes>
+  
+  </BrowserROuter>
+
+
+
+
 };
 
 export default App;
