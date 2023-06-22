5831b1370f477c28a64918d56758d8e05f5ff048
unknown <davedalimocon@gmail.com>
2023-06-23 03:21:53 +0800

    frontend

---
+++ client/vite-client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Header(){
+
+	return (
+		<div className="header" >
+		  <div className="headerList" >
+		   <div className="headerListItem">
+		    <h1>Header</h1>
+		  </div>
+		 </div>
+		</div>
+
+	)
+}
+
+
---
+++ client/vite-client/src/home/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/navbar/Navbar.jsx";
+import Header from "../components/header/Header.jsx";
+
+export default function Home(){
+	
+	return(
+		<>
+		<NavBar/>
+		<Header/>
+		</>
+	)
+
+}
